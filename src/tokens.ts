3b45f5e851864830f1b88feb46c8141ff69efb34
LF Bittencourt <github@lfbittencourt.com>
2024-06-11 10:16:57 -0300

    Add new DoublePipe token

--- src/tokens.ts
+++ src/tokens.ts
@@ -4,6 +4,7 @@ export const Text = createToken({ name: 'Text', pattern: /(\w|\s)+/ });
 export const LeftParenthesis = createToken({ name: 'LeftParenthesis', pattern: '(' });
 export const RightParenthesis = createToken({ name: 'RightParenthesis', pattern: ')' });
 export const QuestionMark = createToken({ name: 'QuestionMark', pattern: '?' });
+export const DoublePipe = createToken({ name: 'DoublePipe', pattern: '||' });
 export const Pipe = createToken({ name: 'Pipe', pattern: '|' });
 
-export default [Text, LeftParenthesis, RightParenthesis, QuestionMark, Pipe];
+export default [Text, LeftParenthesis, RightParenthesis, QuestionMark, DoublePipe, Pipe];
