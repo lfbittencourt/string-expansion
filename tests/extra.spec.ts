04384481783ec24bc95d38c1e8d4001eef01153b
LF Bittencourt <github@lfbittencourt.com>
2024-06-13 09:42:29 -0300

    Update tests to match new token (+)

--- tests/extra.spec.ts
+++ tests/extra.spec.ts
@@ -2,21 +2,24 @@ import expand from '../src';
 
 describe('extra', () => {
   it('should expand complex patterns', () => {
-    const result = expand('Mary( Eli(z|s)abeth)?( Simmons)? Smith(son)?').sort();
+    const result = expand('Mary( Eli(z|s)abeth)?(+ Simmons| Smith(son)?)').sort();
 
     expect(result).toEqual([
-      'Mary Elizabeth Simmons Smithson',
-      'Mary Elizabeth Simmons Smith',
-      'Mary Elizabeth Smithson',
-      'Mary Elizabeth Smith',
-      'Mary Elisabeth Simmons Smithson',
+      'Mary Elisabeth Simmons',
       'Mary Elisabeth Simmons Smith',
-      'Mary Elisabeth Smithson',
+      'Mary Elisabeth Simmons Smithson',
       'Mary Elisabeth Smith',
-      'Mary Simmons Smithson',
+      'Mary Elisabeth Smithson',
+      'Mary Elizabeth Simmons',
+      'Mary Elizabeth Simmons Smith',
+      'Mary Elizabeth Simmons Smithson',
+      'Mary Elizabeth Smith',
+      'Mary Elizabeth Smithson',
+      'Mary Simmons',
       'Mary Simmons Smith',
-      'Mary Smithson',
+      'Mary Simmons Smithson',
       'Mary Smith',
+      'Mary Smithson',
     ].sort());
   });
 });
--- tests/igroups.spec.ts
+++ tests/igroups.spec.ts
@@ -2,24 +2,26 @@ import expand from '../src';
 
 describe('inclusive groups', () => {
   it('should expand inclusive groups', () => {
-    const result = expand('(a||b)').sort();
+    const result = expand('(+a|b)').sort();
 
     expect(result).toEqual(['a', 'b', 'ab'].sort());
   });
 
   it('should expand optional inclusive groups', () => {
-    const result = expand('(a||b)?').sort();
+    const result = expand('(+a|b)?').sort();
 
     expect(result).toEqual(['a', 'b', 'ab', ''].sort());
   });
 
-  it('should expand nested inclusive groups', () => {
-    const result = expand('(a(b||c))').sort();
+  it('should not expand single-option inclusive groups', () => {
+    const result = expand('(+a)').sort();
 
-    expect(result).toEqual(['ab', 'ac', 'abc'].sort());
+    expect(result).toEqual(['a'].sort());
   });
 
-  it('should throw an error when mixing standard and inclusive groups', () => {
-    expect(() => expand('(a|b||c)')).toThrow();
+  it('should expand nested inclusive groups', () => {
+    const result = expand('(+a|(+b|c))').sort();
+
+    expect(result).toEqual(['a', 'ab', 'abc', 'ac', 'b', 'bc', 'c'].sort());
   });
 });
