98b49b433f8a18dee47d45f5f77f7c786fb8cf33
LF Bittencourt <github@lfbittencourt.com>
2024-06-13 09:42:40 -0300

    Update tokens

--- src/tokens.ts
+++ src/tokens.ts
@@ -4,7 +4,7 @@ export const Text = createToken({ name: 'Text', pattern: /(\w|\s)+/ });
 export const LeftParenthesis = createToken({ name: 'LeftParenthesis', pattern: '(' });
 export const RightParenthesis = createToken({ name: 'RightParenthesis', pattern: ')' });
 export const QuestionMark = createToken({ name: 'QuestionMark', pattern: '?' });
-export const DoublePipe = createToken({ name: 'DoublePipe', pattern: '||' });
 export const Pipe = createToken({ name: 'Pipe', pattern: '|' });
+export const PlusSign = createToken({ name: 'PlusSign', pattern: '+' });
 
-export default [Text, LeftParenthesis, RightParenthesis, QuestionMark, DoublePipe, Pipe];
+export default [Text, LeftParenthesis, RightParenthesis, QuestionMark, Pipe, PlusSign];
