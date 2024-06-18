6140374ac136a5bbee7893b18a2aa71364ae53fc
LF Bittencourt <github@lfbittencourt.com>
2024-06-18 14:56:51 -0300

    Implement escaping

--- src/tokens.ts
+++ src/tokens.ts
@@ -6,5 +6,15 @@ export const RightParenthesis = createToken({ name: 'RightParenthesis', pattern:
 export const QuestionMark = createToken({ name: 'QuestionMark', pattern: '?' });
 export const Pipe = createToken({ name: 'Pipe', pattern: '|' });
 export const PlusSign = createToken({ name: 'PlusSign', pattern: '+' });
+export const Backslash = createToken({ name: 'Backslash', pattern: '\\' });
 
-export default [Text, LeftParenthesis, RightParenthesis, QuestionMark, Pipe, PlusSign];
+export const escapableTokens = [
+  Backslash,
+  LeftParenthesis,
+  Pipe,
+  PlusSign,
+  QuestionMark,
+  RightParenthesis,
+];
+
+export default [...escapableTokens, Text];
